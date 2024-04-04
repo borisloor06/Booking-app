ba9b4768c8a02a532ce544630051c7fd1c7d7e76
borisloor06 <boris.loor06@gmail.com>
2024-04-03 22:13:08 -0500

    Agrega navegación dinámica a la barra de navegación

--- src/Layout/Navigation/Navigation.tsx
+++ src/Layout/Navigation/Navigation.tsx
@@ -1,5 +1,14 @@
+import { Pages } from "../../consts/Pages";
 function Navigation() {
-  return <nav>Navigation</nav>;
+  return (
+    <nav>
+      {Pages.map((page) => (
+        <a key={page.id} href={page.path} style={{ margin: "0 10px" }}>
+          {page.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default Navigation;
---
+++ src/consts/Pages.ts
@@ -0,0 +1,12 @@
+export const Pages = [
+  {
+    id: 1,
+    name: "Reservar",
+    path: "/reservar",
+  },
+  {
+    id: 2,
+    name: "Turnos",
+    path: "/turnos",
+  },
+];
