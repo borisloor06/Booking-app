2fdc74968e27e3c84aa5f79ab06acfc86e680dfd
borisloor06 <boris.loor06@gmail.com>
2024-04-03 23:23:07 -0500

    Añadir rutas de reserva y mis reservas en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { getCategories } from "./services/getCategories";
 import { getServicesByCategory } from "./services/getServicesByCategory";
 import { getSlotsByService } from "./services/getSlotsByService";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Layout from "./Layout/Layout";
 
@@ -12,10 +12,12 @@ function App() {
       getSlotsByService(services[0].id).then((slots) => console.log(slots))
     )
   );
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/reservar" />} />
           <Route path="/reservar" element={<div>Reservar</div>} />
           <Route path="/turnos" element={<div>Turnos</div>} />
         </Route>
