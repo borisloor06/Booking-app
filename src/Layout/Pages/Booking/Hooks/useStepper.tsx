ec7070e063cdb5d8ae1c375f9580807d8c78ec51
borisloor06 <boris.loor06@gmail.com>
2024-04-06 14:34:29 -0500

    Actualizar el uso del contexto de reserva y el controlador de página en useStepper.tsx

--- src/Layout/Pages/Booking/Hooks/useStepper.tsx
+++ src/Layout/Pages/Booking/Hooks/useStepper.tsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
+import { useContext } from "react";
 import useSaveBook from "./useSaveBook";
+import {
+  BookingContext,
+  BookingContextType,
+} from "../../BookingProvider/BookingProvider";
 
 export const useStepper = () => {
-  const [page, setPage] = useState<number>(0);
   const pages = [
     "Seleccionar servicio",
     "Seleccionar horario",
     "Confirmar turno",
   ];
   const { saveBook } = useSaveBook();
+  const {
+    page,
+    setPage,
+  } = useContext(BookingContext) as BookingContextType;
 
   const handleNext = () => {
     if (page === pages.length - 1) {
       saveBook();
       return;
     }
-    setPage(page + 1);
+    const newPage = page + 1;
+    setPage(newPage);
   };
 
   const handlePreview = () => {
-    setPage(page - 1);
+    const newPage = page - 1;
+    setPage(newPage);
   };
 
-  return { page, pages, handleNext, handlePreview, setPage };
+  return { pages, handleNext, handlePreview, };
 };
 
 export default useStepper;
--- src/Layout/Pages/BookingProvider/BookingProvider.tsx
+++ src/Layout/Pages/BookingProvider/BookingProvider.tsx
@@ -4,12 +4,16 @@ import { Booking } from "../../../Interfaces/Booking";
 export type BookingContextType = {
   booking: Booking;
   bookings: Booking[];
+  page: number;
+  setPage: (page: number) => void;
   setBookingWithUpdater: (updater: (prev: Booking) => Booking) => void;
   setBookingsWithUpdater: (updater: (prev: Booking[]) => Booking[]) => void;
 };
 export const BookingContext = createContext<BookingContextType>({
   booking: {} as Booking,
   setBookingWithUpdater: () => {},
+  page: 0,
+  setPage: () => {},
   bookings: [],
   setBookingsWithUpdater: () => {},
 });
@@ -17,6 +21,7 @@ export const BookingContext = createContext<BookingContextType>({
 export const BookingProvider = ({ children }: { children: ReactNode }) => {
   const [booking, setBooking] = useState({} as Booking);
   const [bookings, setBookings] = useState([] as Booking[]);
+  const [page, setPage] = useState(0);
   const setBookingWithUpdater = (updater: (prev: Booking) => Booking) => {
     setBooking(updater);
   };
@@ -29,8 +34,10 @@ export const BookingProvider = ({ children }: { children: ReactNode }) => {
       setBookingWithUpdater,
       bookings,
       setBookingsWithUpdater,
+      page,
+      setPage,
     }),
-    [booking, bookings]
+    [booking, bookings, page]
   );
   return (
     <BookingContext.Provider value={contextValue}>
