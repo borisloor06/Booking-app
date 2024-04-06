cbecf0550b2bc3f7d14cf3958acdd29dcf584b82
borisloor06 <boris.loor06@gmail.com>
2024-04-06 08:56:43 -0500

    Actualizar componente DropdownCard para usar el componente Button

--- src/Components/DropdownCard/DropdownCard.tsx
+++ src/Components/DropdownCard/DropdownCard.tsx
@@ -1,20 +1,27 @@
 import { Service } from "../../Interfaces/Services";
+import Button from "../Button/Button";
 
 interface DropdownCardProps {
   option: Service;
   onOptionSelect: (option: number) => void;
+  isSelected: boolean;
 }
 
-const DropdownCard = ({ option, onOptionSelect }: DropdownCardProps) => {
+const DropdownCard = ({
+  option,
+  onOptionSelect,
+  isSelected,
+}: DropdownCardProps) => {
   const handleClick = () => {
     onOptionSelect(option.id);
   };
-
   return (
     <article className="border p-2 m-2 flex flex-col justify-around bg-white">
       <h3>{option.name}</h3>
       <p>{option.description}</p>
-      <button onClick={handleClick} className="ms-auto">Seleccionar</button>
+      <Button selected={isSelected} onClick={handleClick} className="ms-auto">
+        {isSelected ? "Seleccionado" : "Seleccionar"}
+      </Button>
     </article>
   );
 };
