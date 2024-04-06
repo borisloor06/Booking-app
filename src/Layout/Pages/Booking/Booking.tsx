3cc51aaf771806b0ee7475b4501181872f3f0c4a
borisloor06 <boris.loor06@gmail.com>
2024-04-05 23:28:55 -0500

    Agrega barra de progreso y pasos en la página de reserva

--- src/Layout/Pages/Booking/Booking.tsx
+++ src/Layout/Pages/Booking/Booking.tsx
@@ -3,12 +3,26 @@ import SelectService from "./Steps/SelectService/SelectService";
 
 function Booking() {
   const [page, setPage] = useState<number>(0);
-
+  const pages = [
+    "Seleccionar servicio",
+    "Seleccionar horario",
+    "Confirmar turno",
+  ];
   return (
     <>
-      {page === 0 && (
-        <SelectService />
-      )}
+      {/** Add stepper*/}
+      <section className="w-full p-2">
+        <span className="text-slate-800 p-0 m-0 text-xs font-bold">{pages[page]}</span>
+        <div className="w-full bg-slate-50 pt-0">
+          <div
+            className="h-2 bg-teal-500"
+            style={{
+              width: page === 0 ? "33.3%" : page == 1 ? "66.6%" : "100%",
+            }}
+          ></div>
+        </div>
+      </section>
+      {page === 0 && <SelectService />}
       {/** Add footer and steps*/}
     </>
   );
