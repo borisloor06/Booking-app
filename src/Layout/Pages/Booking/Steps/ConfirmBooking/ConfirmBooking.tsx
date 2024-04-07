eedd4ef38a7217669de4f6d96e0f71eb2104f7f9
borisloor06 <boris.loor06@gmail.com>
2024-04-06 19:07:50 -0500

    Corregir formato de fecha y servicio nulo en ConfirmBooking.tsx

--- src/Layout/Pages/Booking/Steps/ConfirmBooking/ConfirmBooking.tsx
+++ src/Layout/Pages/Booking/Steps/ConfirmBooking/ConfirmBooking.tsx
@@ -1,11 +1,11 @@
 import { Booking } from "../../../../../Interfaces/Booking";
 
 function ConfirmBooking({ booking: { service, slot } }: { booking: Booking }) {
-  const formatedDate = slot.date.replace(new RegExp("-", "g"), "/");
+  const formatedDate = slot?.date.replace(new RegExp("-", "g"), "/");
   return (
     <article className="grid grid-cols-1 gap-4 p-4 text-slate-600 font-semibold border-2 text-sm">
       <section className="grid grid-cols-1 gap-2">
-        <p>Servicio: {service.description}</p>
+        <p>Servicio: {service?.description}</p>
         <p>
           Fecha: {formatedDate} {slot?.time}
         </p>
